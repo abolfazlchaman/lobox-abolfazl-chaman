65288c7f35deb9f78103c166c7ff93fb10c20c8f
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-01 21:07:02 +0330

    added robot font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
 
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
 export const metadata: Metadata = {
   title: "Lobox technical assesment - Abolfazl chaman",
   description: "A dropdown selectable menu",
@@ -11,7 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={roboto.className}>
       <body>{children}</body>
     </html>
   );
