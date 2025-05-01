640fdeb24dd516563c36248b74bc6ab7e4217ce0
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-01 18:13:27 +0330

    added use client and the options objetc array for use in the dropdown component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return <div>test, i render correctly ! route : /</div>;
+const options = [
+  { label: "Education", emoji: "🎓" },
+  { label: "Yeeeah, science!", emoji: "🧪" },
+  { label: "Art", emoji: "🎨" },
+  { label: "Sport", emoji: "⚽" },
+  { label: "Games", emoji: "🎮" },
+  { label: "Health", emoji: "🏥" },
+];
+
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Multi-select Dropdown</h1>
+    </main>
+  );
 }
